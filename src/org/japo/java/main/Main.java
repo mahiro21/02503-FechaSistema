3c0b7df6ffaf34d8b9a522b967e18aa587f1565b
mahiro21 <you@example.com>
2019-10-28 17:49:25 +0100

    :zap: fix licenese

--- src/org/japo/java/main/Main.java
+++ src/org/japo/java/main/Main.java
@@ -1,5 +1,5 @@
 /* 
- * 
+ * Copyright 2019 Mario Merlos Abella <mario.merlos.alum@iescamp.es>.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
